7ceee5daaaea2e5a1dc260075f2dd52c27d973bf
WinterWolf08 <sam2604green08@gmail.com>
2025-03-28 23:43:53 -0500

    Update PayPalAdapter.java
    restricciones

--- PayPalAdapter.java
+++ PayPalAdapter.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit este template
  */
 package co.poli.edu.ejemplo1.modelo.payment;
 
@@ -10,6 +10,9 @@ package co.poli.edu.ejemplo1.modelo.payment;
 public class PayPalAdapter implements PaymentAdapter {
     @Override
     public String processPayment(final double amount) {
+        if (amount <= 0) {
+            return "El monto debe ser mayor que cero.";
+        }
         // Lógica para procesar el pago con PayPal
         return "Pago de " + amount + " procesado con PayPal.";
     }
