020e0e91398425dc8399098526164175a4aee53d
WinterWolf08 <sam2604green08@gmail.com>
2025-03-28 23:22:25 -0500

    Update Project.java
    pulir documentación y agregar final a variables que no cambian

--- Project.java
+++ Project.java
@@ -12,33 +12,34 @@ import co.poli.edu.ejemplo1.modelo.payment.PayPalAdapter;
 import co.poli.edu.ejemplo1.modelo.payment.PaymentAdapter;
 
 /**
- *
- * @author USUARIO
+ * Clase principal para ejecutar el proyecto.
+ * 
+ * @autor USUARIO
  */
 public class Project {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // Crear instancias de Nequi y PayPal
-        Nequi nequi = new Nequi();
-        PayPal paypal = new PayPal();
+        final Nequi nequi = new Nequi();
+        final PayPal paypal = new PayPal();
 
         // Crear adaptadores para Nequi y PayPal
-        PaymentAdapter nequiAdapter = new NequiAdapter(nequi);
-        PaymentAdapter paypalAdapter = new PayPalAdapter(paypal);
+        final PaymentAdapter nequiAdapter = new NequiAdapter(nequi);
+        final PaymentAdapter paypalAdapter = new PayPalAdapter(paypal);
 
         // Crear una instancia de Pedido
-        Pedido pedido = new Pedido();
+        final Pedido pedido = new Pedido();
 
         // Configurar y procesar el pago con Nequi
-        String resultadoNequi = procesarPago(pedido, nequiAdapter, 100.0);
+        final String resultadoNequi = procesarPago(pedido, nequiAdapter, 100.0);
         System.out.println(resultadoNequi);
 
         // Configurar y procesar el pago con PayPal
-        String resultadoPayPal = procesarPago(pedido, paypalAdapter, 200.0);
+        final String resultadoPayPal = procesarPago(pedido, paypalAdapter, 200.0);
         System.out.println(resultadoPayPal);
     }
 
-    private static String procesarPago(Pedido pedido, PaymentAdapter adapter, double monto) {
+    private static String procesarPago(final Pedido pedido, final PaymentAdapter adapter, final double monto) {
         pedido.setPaymentAdapter(adapter);
         pedido.processPayment(monto);
         return "Pago procesado con " + adapter.getClass().getSimpleName() + ": $" + monto;
