94153a3482de95fd9dbaa823def03d97acdb9bb1
WinterWolf08 <sam2604green08@gmail.com>
2025-03-28 23:18:11 -0500

    Update Pedido.java
    pulir líneas de código

--- Pedido.java
+++ Pedido.java
@@ -2,50 +2,37 @@ package co.poli.edu.ejemplo1.modelo.Model.co.poli.edu.ejemplo1.modelo;
 
 import co.poli.edu.ejemplo1.modelo.Cliente;
 import co.poli.edu.ejemplo1.modelo.payment.PaymentAdapter;
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 /**
+ * Clase que representa un pedido.
  * 
  */
 public class Pedido {
 
     /**
-     * Default constructor
+     * Constructor por defecto.
      */
     public Pedido() {
     }
 
-    /**
-     * 
-     */
     private String numero;
-
-    /**
-     * 
-     */
     private String fecha;
-
-    /**
-     * 
-     */
     private Cliente cliente;
-
-    /**
-     * 
-     */
     private ArrayList<Producto> producto;
-
-    /**
-     * 
-     */
     private PaymentAdapter paymentAdapter;
 
-    public void setPaymentAdapter(PaymentAdapter paymentAdapter) {
+    public void setPaymentAdapter(final PaymentAdapter paymentAdapter) {
         this.paymentAdapter = paymentAdapter;
     }
 
-    public String processPayment(double amount) {
+    /**
+     * Procesa el pago del pedido.
+     * 
+     * @param amount El monto a pagar.
+     * @return Un mensaje indicando el resultado del pago.
+     */
+    public String processPayment(final double amount) {
         if (paymentAdapter != null) {
             paymentAdapter.processPayment(amount);
             return "Payment processed with amount: " + amount;
